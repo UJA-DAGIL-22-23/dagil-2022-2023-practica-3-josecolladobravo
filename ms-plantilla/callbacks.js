396ebcd567144e5368a70a8822011010877d579c
jose collado <josecolladobravo@gmail.com>
2023-04-18 08:28:04 +0200

    1. HU Acerca De

--- ms-plantilla/callbacks.js
+++ ms-plantilla/callbacks.js
@@ -93,9 +93,9 @@ const CB_OTHERS = {
         try {
             CORS(res).status(200).json({
                 mensaje: "Microservicio MS Plantilla: acerca de",
-                autor: "¿¿¿ AUTOR ???",
-                email: "¿¿¿ EMAIL ???",
-                fecha: "¿¿¿ FECHA ???"
+                autor: "José Collado Bravo",
+                email: "jcb00029@red.ujaen.es",
+                fecha: "17/04/2023"
             });
         } catch (error) {
             CORS(res).status(500).json({ error: error.description })
